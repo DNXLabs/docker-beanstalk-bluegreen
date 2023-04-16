c56bd73c7c26a03933ca05d9fc887b7ae76fbbfb
Caio Fernandes <caiovf13@gmail.com>
2023-04-16 11:51:20 -0300

    👌 IMPROVE: Code organization

--- src/swap_environment.py
+++ src/swap_environment.py
@@ -1,5 +1,6 @@
 import json
 import time
+import aws_authentication
 
 
 
@@ -9,7 +10,7 @@ def main(BLUE_ENV_NAME, GREEN_ENV_NAME, S3_ARTIFACTS_BUCKET, BEANSTALK_APP_NAME,
     route53_client = boto_authenticated_client.client('route53', region_name='ap-southeast-2')
     ssm_client = boto_authenticated_client.client('ssm', region_name='ap-southeast-2')
 
-    BLUE_CNAME_CONFIG_FILE = "blue_green_assets/blue_cname.json"
+    BLUE_CNAME_CONFIG_FILE = "src/blue_cname.json"
 
     blue_env_url = get_env_address(BLUE_CNAME_CONFIG_FILE, S3_ARTIFACTS_BUCKET, s3client)
     print("Blue env URL: " + str(blue_env_url))
