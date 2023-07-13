e2144eb687f2d642e93bb06488c919a7e23c4535
Caio Fernandes <caiovf13@gmail.com>
2023-07-12 21:10:18 -0300

    Add environment validation before rollbakc

--- src/deploy_release.py
+++ src/deploy_release.py
@@ -1,6 +1,7 @@
 from time import strftime, sleep
 from botocore.exceptions import ClientError
 import os
+import swap_environment
 
 def main(BUCKET_KEY, S3_ARTIFACTS_BUCKET, BLUE_ENV_NAME, BEANSTALK_APP_NAME, boto_authenticated_client):
     VERSION_LABEL = strftime("%Y%m%d%H%M%S")
@@ -96,6 +97,14 @@ def rollback_release(client, application_name, environment_name):
     except ClientError as err:
         print("Failed to create boto3 beanstalk client.\n" + str(err))
         return False
+    environment_info, client = swap_environment.get_environment_information(
+        beanstalkclient, environment_name)
+
+    while environment_info["Environments"][0]["Status"] != "Ready":
+        sleep(10)
+        environment_info, client = swap_environment.get_environment_information(
+            beanstalkclient, environment_name)
+
     response = beanstalkclient.describe_application_versions(
         ApplicationName=application_name,
         MaxRecords=2
